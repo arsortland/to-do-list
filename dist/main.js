e32e8e7a3537c8a60fb44bd554d17062bea5b04c
André Sortland <arsortland@gmail.com>
2022-03-28 23:48:14 +0200

    playing around with factories and classes

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://to-do-list/./src/index.js?");
+eval("const toDo = (title, description = \"\", notes = \"\", date, priority) => {\n  return {\n    title,\n    description,\n    notes,\n    date,\n    priority,\n  };\n};\n\nconst objektet = toDo(\"EnTittel\", \"enBeskrivelse\", \"Notater\", \"dato\", \"Pri#1\");\nconsole.log(objektet.title);\n\nobjektet.title = \"EnAnnenTittel\";\nconsole.log(objektet.title);\n\n// class ToDO {\n//   constructor(title, description, notes, date, priority) {\n//     this.title = title;\n//     this.description = description;\n//     this.notes = notes;\n//     this.date = date;\n//     this.priority = priority;\n//   }\n//   get getTitle() {\n//     return this.title;\n//   }\n\n//   set changeTitle(newTitle) {\n//     this.title = newTitle;\n//   }\n\n//   changeTitle(newOne) {\n//     this._title = newOne;\n//     return;\n//   }\n// }\n\n// const user = new ToDO(\n//   \"EnTittel\",\n//   \"EnBeskrivelse\",\n//   \"EtNotat\",\n//   \"DatoXX\",\n//   \"Pri#1\"\n// );\n\n// console.log(user.title);\n// user.title = \"Gunnar\";\n\n// console.log(user.title);\n\n\n//# sourceURL=webpack://to-do-list/./src/index.js?");
 
 /***/ })
 
